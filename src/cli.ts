b63f88e1a10e8ef615ab80ef040fc4560a8ad740
Matt Gadda <mgadda@gmail.com>
2018-04-05 11:19:31 -0700

    Add support for --config

--- src/cli.ts
+++ src/cli.ts
@@ -1,33 +1,63 @@
 import * as program from "commander"
 import { Server } from "./server";
+import { readFileSync, readFile } from "fs";
+import { promisify } from "util";
 
+export async function main(): Promise<void> {
+  let httpPort: number;
 
-let httpPort: number;
+  program
+    .name("airquality-server")
+    .version("0.1.0")
+    .option("-v, --verbose", "Be more verbose", false)
+    .option("-p, --port <n>", "port", parseInt, 4000)
+    .option("--database-path <s>", "Path to sqlite database", "./aq.db")
+    .option("--test-mode", "Generate fake database from fake device /dev/ROBOT")
+    .option(
+      "-s, --sampling-period <n>", 
+      "Number of minutes between saving samples to disk", 
+      (value) => parseFloat(value) * 60 * 1000, 
+      15.0)
+    .option("-d, --device <s>", "Serial device to read from")
+    .option("--config <s>", "Path to json config file")
+    .parse(process.argv);
 
-program
-  .name("airquality-server")
-  .version("0.1.0")
-  .option("-v, --verbose", "Be more verbose")
-  .option("-p, --port <n>", "port", parseInt, 4000)
-  .option("--database-path <s>", "Path to sqlite database", "./aq.db")
-  .option("--test-mode", "Generate fake database from fake device /dev/ROBOT")
-  .option(
-    "-s, --sampling-period <n>", 
-    "Number of minutes between saving samples to disk", 
-    (value) => parseFloat(value) * 60 * 1000, 
-    15.0)
-  .option("-d, --device <s>", "Serial device to read from", "/dev/cu.usbmodem1421")
-  .parse(process.argv);
+  let server: Server;
 
+  if (program.config) {
+    const file = await promisify(readFile)(program.config);
+    const config = JSON.parse(file.toString());
+    
+    if (!config) {
+      throw new Error("Malformed config: " + program.config);
+    } else if (!config.device) {
+      throw new Error("`device` is a required property");
+    }
 
-const server = new Server(
-  program.verbose, 
-  program.port, 
-  program.databasePath, 
-  program.samplingPeriod,
-  program.device,
-  program.testMode,
-);
+    server = new Server(
+      config.verbose || false, 
+      config.port || 4000, 
+      config.databasePath || "./aq.db", 
+      config.samplingPeriod ? config.samplingPeriod * 60 * 1000 : 15.0,
+      config.device,
+      config.testMode || false,
+    );
 
-server.run();
+  } else {
+    server = new Server(
+      program.verbose, 
+      program.port, 
+      program.databasePath, 
+      program.samplingPeriod,
+      program.device,
+      program.testMode,
+    );
+  }
 
+  server.run();
+}
+
+main().catch((error) => { 
+  console.log(error.message);
+  process.exit(1);
+});
